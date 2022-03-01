c1ed3dea515b59c9bf46e03bad2cb826ac6dcf09
Gema Fernández-Avilés <gema.faviles@uclm.es>
2022-03-01 09:30:49 +0100

    # actualizo mapas contorno

--- Slides_geor_v1.0.R
+++ Slides_geor_v1.0.R
@@ -2,15 +2,92 @@
 # opciones predeterminadas
 options(htmltools.dir.version = FALSE)
 knitr::opts_chunk$set(
-  fig.width=9, fig.height=3.5, fig.retina=3,
-  out.width = "80%",
+  fig.width=7, fig.height=6, 
+  fig.retina=3,
+ # out.width = "30%",
+ # out.height="30%",
   echo = TRUE,
   message = FALSE, 
   warning = FALSE,
   fig.show = TRUE,
   hiline = TRUE,
+  cache = FALSE,
   fig.align = "center"
   )
+
+
+
+## ----cod_datos, eval=FALSE-----------------------------------------------------------------------------------
+## library(geoR)
+## # `mygeodata` es un objeto espacial: $coords y $data
+## mygeodata <- as.geodata(mydata, coords.col = 1:2, data.col = 3)
+
+
+## ----cod_eda, eval=FALSE-------------------------------------------------------------------------------------
+## summary(mygeodata)
+## plot(mygeodata)
+
+
+## ----cod_sem_emp, eval=FALSE---------------------------------------------------------------------------------
+## semivar_emp <- variog(mygeodata, max.dist = 2/3*distancia_maxima_coordenadas)
+## plot(semivar_emp)
+
+
+## ----cod_sem_teo, eval=FALSE---------------------------------------------------------------------------------
+## semivar_teo <- eyefit(semivar_emp) # función interactiva
+## semivar_teo # contiene los parámetros del semivariograma teórico
+##     cov.model sigmasq  phi tausq kappa kappa2   practicalRange
+
+
+## ----cod_inter, eval=FALSE-----------------------------------------------------------------------------------
+## * `cov.model` = modelo de covarianza
+## * `sigmasq` = varianza parcial
+## * `phi` = rango o alcance
+## * `tausq` = nugget
+## * `practicalRange`= distancia en la que se estabiliza el semivariograma
+
+
+## ----cod_krig, eval=FALSE------------------------------------------------------------------------------------
+## xx <- seq(min, max, l = 51)  #min y #máx para el eje x
+## yy <- seq(min, max, l = 51)  #min y #máx para el eje y
+## grid_prediccion <- expand.grid(x = xx, y = yy)
+## 
+## # help(krige.conv)
+## krig_ord <- krige.conv(mygeodata, #datos
+##                        loc = grid_prediccion, #grid de predicción
+##                        krige = krige.control(obj.m = semivar_teo) #semivariograma teórico
+##                        )
+## names(krig_ord)
+
+
+## ----cod_plots, eval=FALSE-----------------------------------------------------------------------------------
+## #varias funciones de mapeado
+## contour(krig_ord, filled = TRUE)
+## image(krig_ord, val = krig_ord$krige.var) #superficie de varianzas
+## 
+## # validación cruzada
+## mygeodata_xv <- xvalid(mygeodata, model = semivar_teo)
+
+
+## ----cod_3dplot, eval=FALSE----------------------------------------------------------------------------------
+## library(plot3D)
+## # install.packages('plot3D')
+## persp3D(xx, yy, matrix(krig_ord$predict, nrow = length(xx)), theta=-60, phi=40)
+
+
+## ----ms_madrid, echo=FALSE-----------------------------------------------------------------------------------
+MuniMadrid <- matrix(scan("data/MuniMadrid.txt"), ncol = 2, byrow = T)
+data.co=read.table("data/Madrid_LOG_co50s10h.txt", header=TRUE)
+
+library(dplyr); library(sf); library(leaflet)
+
+MuniMadrid_sf <- st_as_sf(as.data.frame(MuniMadrid), coords = c("V1","V2"),
+                          crs=st_crs(25830)) %>% st_transform(4326)
+ha <- st_as_sf(data.co, coords = c("x","y"), crs=st_crs(25830)) %>%  st_transform(4326)
+dato= data.co$co
+dato <- sprintf("<strong>Level of CO: %s</strong>", round(data.co$co,2)) %>% lapply(htmltools::HTML)
+leaflet(ha) %>% addTiles() %>% addMarkers(popup = dato, clusterOptions = markerClusterOptions()) %>%
+ addCircleMarkers(radius = 7, color = "red", popup = dato)
 
 
 ## ----leo_datos, echo=TRUE------------------------------------------------------------------------------------
@@ -36,18 +113,6 @@ plot(co.50s.10h)
 
 # Mapa de quintiles
 points(co.50s.10h, pch=21, bg=8, lwd=4, cex.max=3, col=cm.colors(12) )
-
-
-## ----ms_madrid-----------------------------------------------------------------------------------------------
-library(dplyr); library(sf); library(leaflet)
-
-MuniMadrid_sf <- st_as_sf(as.data.frame(MuniMadrid), coords = c("V1","V2"),
-                          crs=st_crs(25830)) %>% st_transform(4326)
-ha <- st_as_sf(data.co, coords = c("x","y"), crs=st_crs(25830)) %>%  st_transform(4326)
-dato= data.co$co
-dato <- sprintf("<strong>Level of CO: %s</strong>", round(data.co$co,2)) %>% lapply(htmltools::HTML)
-leaflet(ha) %>% addTiles() %>% addMarkers(popup = dato, clusterOptions = markerClusterOptions()) %>%
- addCircleMarkers(radius = 7, color = "red", popup = dato)
 
 
 ## ----co_sem_empirico-----------------------------------------------------------------------------------------
@@ -116,18 +181,15 @@ kc.co.2s.10h <- krige.conv(co.50s.10h,
 str(kc.co.2s.10h)
 
 
-## ----co_krig_sd, cecho=FALSE, include=FALSE------------------------------------------------------------------
-par(mfrow = c(1, 2), mar = c(3.5, 3.5, 1, 0), mgp = c(1.5, 0.5, 0))
-image(kc.co.2s.10h, borders = MuniMadrid, main = "2D prediction map", 
-      ylim = c(4461000, 4499622), col = rainbow(256))  # 
-points(estaciones, col = 1, lwd = 1)
-legend.krige(x.leg = c(424854, 456141), y.leg = c(4461000, 4462200), kc.co.2s.10h$predict, 
-    col = rainbow(256))
-image(kc.co.2s.10h, borders = MuniMadrid, val = sqrt(kc.co.2s.10h$krige.var), 
-    main = "Prediction SD map", ylim = c(4461000, 4499622), col = terrain.colors(64))
-points(estaciones, col = 1, lwd = 3)
-legend.krige(x.leg = c(424854, 456141), y.leg = c(4461000, 4462200), sqrt(kc.co.2s.10h$krige.var), 
-    col = terrain.colors(64))
+## ----co_krig_sd----------------------------------------------------------------------------------------------
+# prediction map
+contour(kc.co.2s.10h,  borders = MuniMadrid, filled=TRUE, cex=1, col=terrain.colors(20))
+
+# sd map
+contour(kc.co.2s.10h,  values = sqrt(kc.co.2s.10h$krige.var), 
+        borders = MuniMadrid, filled=TRUE)
+
+
 
 
 ## ----co_3D, echo=FALSE, include=FALSE------------------------------------------------------------------------
@@ -136,7 +198,7 @@ persp(kc.co.2s.10h, borders = MuniMadrid, main = "3D Prediction map", theta = 0,
     phi = 40, expand = 0.5, col = "green")
 
 
-## ----co_leaflet, echo=FALSE, fig.height = 5------------------------------------------------------------------
+## ----co_leaflet, echo=FALSE----------------------------------------------------------------------------------
 
 # Paso la predicción a raster y lo proyecto a 4326
 
@@ -190,8 +252,8 @@ leaflet() %>%
 
 
 
-## ----eval=FALSE, include=FALSE, message=FALSE, results='hide', echo=FALSE------------------------------------
+## ----saca_cod_r, eval=FALSE, include=FALSE, message=FALSE, results='hide', echo=FALSE------------------------
 ## library(knitr)
 ## knit('Slides_geor_v1.0.Rmd', tangle=TRUE)
-## source('cod_Slides_geor_v.1.0.R')
+## source('Slides_geor_v1.0.R')
 
